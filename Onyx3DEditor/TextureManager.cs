235676bf38758ec7d95284052f1ed1849e6eed35
Josue Candela Perdomo <josuecp88@gmail.com>
2017-11-01 13:16:52 +0000

    Improved Texture Manager with image list

--- Onyx3DEditor/TextureManager.cs
+++ Onyx3DEditor/TextureManager.cs
@@ -12,6 +12,8 @@ namespace Onyx3DEditor
 	public partial class TextureManager : Form
 	{
 		private TextureDBEntry mCurrentTexture;
+		//private TextureDBEntry mChangedTexture;
+
 
 		public TextureManager()
 		{
@@ -21,62 +23,112 @@ namespace Onyx3DEditor
 
 		private void FillTexturesList()
 		{
-			toolStripComboBoxTextures.Items.Clear();
+			listViewTextures.Items.Clear();
+			int i = 0;
+
+			
+			//listViewTextures.LargeImageList = new ImageList();
+			//listViewTextures.LargeImageList.ImageSize = new Size(128, 128);
+			listViewTextures.SmallImageList = new ImageList();
+			listViewTextures.SmallImageList.ImageSize = new Size(64,64);
 			foreach (TextureDBEntry t in ProjectManager.Instance.Content.Textures)
 			{
-				toolStripComboBoxTextures.Items.Add(t.id);
+				Bitmap bmp = new Bitmap(t.path);
+				Image small_img = bmp.GetThumbnailImage(64, 64, null, IntPtr.Zero);
+				//Image big_img = bmp.GetThumbnailImage(256, 256, null, IntPtr.Zero);
+				listViewTextures.SmallImageList.Images.Add(small_img);
+				//listViewTextures.LargeImageList.Images.Add(big_img);
+				listViewTextures.Items.Add(new ListViewItem(t.id, i));
+				i++;
 			}
 		}
 
 		private void toolStripButtonOpen_Click(object sender, EventArgs e)
 		{
-			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+			string filePath = OpenFileSelector(ProjectManager.Instance.CurrentProjectPath);
+			if (filePath.Length > 0)
+				CreateNewTexture(filePath);
+		}
 
-			openFileDialog1.InitialDirectory = "c:\\";
+		private string OpenFileSelector(string initPath)
+		{
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+			openFileDialog1.InitialDirectory = initPath;
 			openFileDialog1.Filter = "PNG files (*.png)|*.png|JPG files (*.jpg)|*.jpg";//All files (*.*)|*.*
 			openFileDialog1.FilterIndex = 2;
 			openFileDialog1.RestoreDirectory = true;
-
 			if (openFileDialog1.ShowDialog() == DialogResult.OK)
 			{
-				try
-				{
-					
-					mCurrentTexture = new TextureDBEntry();
-					mCurrentTexture.path = openFileDialog1.FileName;
-					mCurrentTexture.id = "NONE";
-					ProjectManager.Instance.Content.Textures.Add(mCurrentTexture);
-
-					UpdateTextureInfo();
-				}
-				catch (Exception ex)
-				{
-					MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
-				}
+				return openFileDialog1.FileName;
 			}
+			return "";
+		}
+
+		private void CreateNewTexture(string path)
+		{
+			mCurrentTexture = new TextureDBEntry();
+			mCurrentTexture.path = path;
+			mCurrentTexture.id = "NONE";
+			ProjectManager.Instance.Content.Textures.Add(mCurrentTexture);
+
+			UpdateTextureInfo();
+			FillTexturesList();
 		}
 
 		private void UpdateTextureInfo()
 		{
+			textBoxFilePath.Show();
+			textBoxId.Show();
 			textBoxFilePath.Text = mCurrentTexture.path;
 			textBoxId.Text = mCurrentTexture.id;
-			Bitmap bmp = new Bitmap(mCurrentTexture.path);
+			LoadTexturePreview();
+		}
+
+		private void LoadTexturePreview()
+		{
+			Bitmap bmp = new Bitmap(textBoxFilePath.Text);
 			pictureBoxPreview.Image = bmp.GetThumbnailImage(256, 256, null, IntPtr.Zero);
 		}
 
-		private void textBoxId_TextChanged(object sender, EventArgs e)
+		private void ClearTextureInfo()
+		{
+			textBoxFilePath.Hide();
+			textBoxId.Hide();
+			pictureBoxPreview.Image = null;
+		}
+
+		
+		private void listViewTextures_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (listViewTextures.SelectedIndices.Count > 0) { 
+				mCurrentTexture = ProjectManager.Instance.Content.Textures[listViewTextures.SelectedIndices[0]];
+				UpdateTextureInfo();
+			}
+			else
+			{
+				ClearTextureInfo();
+			}
+		}
+
+		private void buttonApply_Click(object sender, EventArgs e)
 		{
 			if (mCurrentTexture != null)
 			{
 				mCurrentTexture.id = textBoxId.Text;
+				mCurrentTexture.path = textBoxFilePath.Text;
+				FillTexturesList();
 			}
 		}
 
-		private void toolStripComboBoxTextures_SelectedIndexChanged(object sender, EventArgs e)
+		private void buttonOpen_Click(object sender, EventArgs e)
 		{
-			mCurrentTexture = ProjectManager.Instance.Content.Textures[toolStripComboBoxTextures.SelectedIndex];
-			UpdateTextureInfo();
+			string filePath = OpenFileSelector(textBoxFilePath.Text);
+			if (filePath.Length > 0) {
+				textBoxFilePath.Text = filePath;
+				LoadTexturePreview();
+			}
 		}
+		
 	}
 
 }
