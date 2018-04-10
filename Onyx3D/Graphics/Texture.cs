9ab858ca989bf8e0bb74bb49a37ff5e988cd385d
Josue Candela Perdomo <josuecp88@gmail.com>
2018-04-10 18:05:31 +0100

    Texture to Bitmap conversion

--- Onyx3D/Graphics/Texture.cs
+++ Onyx3D/Graphics/Texture.cs
@@ -14,6 +14,9 @@ namespace Onyx3D
 		private string mPath;
 		private int mId;
 
+		public int Width { get; private set; }
+		public int Height { get; private set; }
+
 		public int Id
 		{
 			get { return mId; } 
@@ -22,6 +25,26 @@ namespace Onyx3D
 		public string Path
 		{
 			get { return mPath; }
+		}
+
+		public Texture(int w, int h)
+		{
+			Width = w;
+			Height = h;
+
+			GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+			GL.GenTextures(1, out mId);
+			GL.BindTexture(TextureTarget.Texture2D, mId);
+			
+			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, w, h, 0,
+				OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero);
+
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
 		}
 
 		public Texture(string file)
@@ -38,6 +61,9 @@ namespace Onyx3D
 			BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
 				ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
+			Width = data.Width;
+			Height = data.Height;
+
 			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
 				OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
 			bitmap.UnlockBits(data);
@@ -46,7 +72,18 @@ namespace Onyx3D
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+		}
 
+		public Bitmap AsBitmap()
+		{
+			Bitmap bitmap = new Bitmap(Width, Height);
+			BitmapData bits = bitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+			GL.BindTexture(TextureTarget.Texture2D, mId);
+			GL.GetTexImage(TextureTarget.Texture2D, 0, OpenTK.Graphics.OpenGL.PixelFormat.Rgba, PixelType.UnsignedByte, bits.Scan0);
+			
+			bitmap.UnlockBits(bits);
+			return bitmap;
 		}
 	}
 }
