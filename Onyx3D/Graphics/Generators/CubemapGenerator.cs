4c22d24725cc7297ccb58744335c833c2973d253
Josue Candela Perdomo <josuecp88@gmail.com>
2018-09-27 20:12:12 +0100

    Removed unnecesary FrameBuffers

--- Onyx3D/Graphics/Generators/CubemapGenerator.cs
+++ Onyx3D/Graphics/Generators/CubemapGenerator.cs
@@ -7,33 +7,31 @@ namespace Onyx3D
     public class CubemapGenerator : IDisposable
     {
       
-
         private Camera mCamera;
-        private FrameBuffer[] mFrameBuffer;
+        private FrameBuffer mFrameBuffer;
         private Vector3[] mCamRotations;
 
         public CubemapGenerator(int size)
         {
             mCamera = new PerspectiveCamera("", (float)Math.PI / 2.0f, 1);
-            mFrameBuffer = new FrameBuffer[6];
-            for(int i=0;i<6;++i)
-                mFrameBuffer[i] = new FrameBuffer(size, size);
+            
+            mFrameBuffer = new FrameBuffer(size, size);
 
             mCamRotations = new Vector3[6];
-            mCamRotations[0] = new Vector3(0, 0, 0);                                                    
-            mCamRotations[1] = new Vector3(0, (float)Math.PI / 2.0f, 0);                                
-            mCamRotations[2] = new Vector3(0, (float)Math.PI, 0);                                       
-            mCamRotations[3] = new Vector3(0, -(float)Math.PI / 2.0f, 0);                               
-            mCamRotations[4] = new Vector3((float)Math.PI / 2.0f, 0, 0);								
-            mCamRotations[5] = new Vector3(-(float)Math.PI / 2.0f, 0, 0);     
+            
+            mCamRotations[0] = new Vector3(0, -(float)Math.PI / 2.0f, 0); // Right
+            mCamRotations[1] = new Vector3(0, (float)Math.PI / 2.0f, 0);  // Left
+            mCamRotations[2] = new Vector3(-(float)Math.PI / 2.0f, 0, 0); // Top
+            mCamRotations[3] = new Vector3((float)Math.PI / 2.0f, 0, 0);  // Bottom
+            mCamRotations[4] = new Vector3(0, 0, 0);                      // Back                                              
+            mCamRotations[5] = new Vector3(0, (float)Math.PI, 0);         // Front                     
+            
         }
 
 		public void Dispose()
 		{
-			for (int i = 0; i < 6; ++i)
-				mFrameBuffer[i].Dispose();
-
-			mCamera.Dispose();
+            mFrameBuffer.Dispose();
+            mCamera.Dispose();
 		}
 
         public void Generate(RenderManager renderMgr, Scene scene, Vector3 position, ref Cubemap cubemap)
@@ -46,26 +44,15 @@ namespace Onyx3D
             {
                 mCamera.Transform.LocalRotation = Quaternion.FromEulerAngles(mCamRotations[i]);
 
-				mFrameBuffer[i].Bind();
-                renderMgr.Render(scene, mCamera, mFrameBuffer[i].Width, mFrameBuffer[i].Height);
-                mFrameBuffer[i].Unbind();
-			}
+				mFrameBuffer.Bind();
+                renderMgr.Render(scene, mCamera, mFrameBuffer.Width, mFrameBuffer.Height);
+                mFrameBuffer.Unbind();
 
-            cubemap.SetTexture(CubemapFace.Left, mFrameBuffer[1].Texture);
-            cubemap.SetTexture(CubemapFace.Right, mFrameBuffer[3].Texture);
-            cubemap.SetTexture(CubemapFace.Front, mFrameBuffer[2].Texture);
-            cubemap.SetTexture(CubemapFace.Back, mFrameBuffer[0].Texture);
-            cubemap.SetTexture(CubemapFace.Top, mFrameBuffer[5].Texture);
-            cubemap.SetTexture(CubemapFace.Bottom, mFrameBuffer[4].Texture);
-
+                cubemap.SetTexture((CubemapFace)i, mFrameBuffer.Texture);
+            }
+            
 			cubemap.GenerateMipmaps();
-
-			cubemap.TextureRight = mFrameBuffer[1].Texture;
-            cubemap.TextureLeft = mFrameBuffer[3].Texture;
-            cubemap.TextureBack = mFrameBuffer[2].Texture;
-            cubemap.TextureFront = mFrameBuffer[0].Texture;
-            cubemap.TextureTop = mFrameBuffer[5].Texture;
-            cubemap.TextureBottom = mFrameBuffer[4].Texture;
+           
         }
     }
 }
